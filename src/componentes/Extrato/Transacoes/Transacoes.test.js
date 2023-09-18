ad392fa1293b3907c8fcef297547926bb5a78c6e
William Souza <william.h.souza@hotmail.com>
2023-09-18 13:21:46 -0300

    v2.0

--- src/componentes/Extrato/Transacoes/Transacoes.test.js
+++ src/componentes/Extrato/Transacoes/Transacoes.test.js
@@ -1,31 +1,30 @@
-import {render, screen} from '@testing-library/react'
-import Transacoes from './index'
-import estilos from '../Extrato.module.css'
+import { render, screen } from '@testing-library/react';
+import Transacoes from './index';
+import estilos from '../Extrato.module.css';
 
-test('Deve redenrizar o mesmo componente com props atualizadas', ()=>{
-    const transacao = {
-        transacao: 'Depósito',
-        valor: 100,
-    }
+test('Deve renderizar o mesmo componente com props atualizadas', () => {
+  const transacao = {
+    transacao: 'Depósito',
+    valor: 100,
+  };
+  const { rerender } = render(
+    <Transacoes estilos={estilos} transacao={transacao} />
+  );
+  const tipoTransacao = screen.getByTestId('tipoTransacao');
+  const valorTransacao = screen.getByTestId('valorTransacao');
 
-    const {rerender} = render(<Transacoes estilos={estilos} transacao={transacao}/>)
-    const tipoTransacao = screen.getByTestId('tipoTransacao')
-    const valorTransacao = screen.getByTestId('valorTransacao')
+  expect(tipoTransacao).toHaveTextContent('Depósito');
+  expect(valorTransacao).toHaveTextContent('R$ 100');
 
-    expect(tipoTransacao).toHaveTextContent('Depósito')
-    expect(valorTransacao).toHaveTextContent('R$ 100')
+  const novaTransacao = {
+    transacao: 'Transferência',
+    valor: 50,
+  };
 
-    const Novatransacao = {
-        transacao: 'Transferência',
-        valor: 50,
-    }
+  rerender(<Transacoes estilos={estilos} transacao={novaTransacao} />);
+  const novoTipoTransacao = screen.getByTestId('tipoTransacao');
+  const novoValorTransacao = screen.getByTestId('valorTransacao');
 
-    rerender(<Transacoes estilos={estilos} transacao={Novatransacao}/>)
-
-    const novoTipoTransacao = screen.getByTestId('tipoTransacao')
-    const novoValorTransacao = screen.getByTestId('valorTransacao')
-
-    expect(novoTipoTransacao).toHaveTextContent('Transferência')
-    expect(novoValorTransacao).toHaveTextContent('- R$ 50')
-
-})
+  expect(novoTipoTransacao).toHaveTextContent('Transferência');
+  expect(novoValorTransacao).toHaveTextContent('- R$ 50');
+});
--- src/componentes/Principal/Saldo/Saldo.test.js
+++ src/componentes/Principal/Saldo/Saldo.test.js
@@ -1,11 +1,11 @@
-import {render, screen} from '@testing-library/react'
-import Saldo from './index'
+import { render, screen } from '@testing-library/react';
+import Saldo from './index';
 
-describe('Componente <Saldo/>', ()=>{
-    test('Deve renderizar o saldo com o valor monetário', ()=> {
-        render(<Saldo saldo={1000}/>)
+describe('Componente <Saldo />', () => {
+  test('Deve renderizar o saldo com valor monetário', () => {
+    render(<Saldo saldo={1000} />);
 
-        const saldo = screen.getByTestId('saldo')
-        expect(saldo).toHaveTextContent('R$ 1000')
-    })
-})
+    const saldo = screen.getByTestId('saldo');
+    expect(saldo).toHaveTextContent('R$ 1000');
+  });
+});

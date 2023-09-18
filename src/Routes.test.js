a26b5954d855e3cc9f375c16c7849a4feda77d00
William Souza <william.h.souza@hotmail.com>
2023-09-18 15:28:42 -0300

    v2.4

--- src/Routes.test.js
+++ src/Routes.test.js
@@ -1,11 +1,27 @@
 import { render, screen } from "@testing-library/react";
 import App from './paginas/Principal/App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter, Routes, Route } from "react-router-dom";
+import Cartoes from './componentes/Cartoes';
 
 describe('Rotas',()=>{
     test('Deve Renderizar a rota principal', ()=>{
         render(<App/>, {wrapper: BrowserRouter});
         const usuario = screen.getByText('Olá, Joana :)!');
         expect(usuario).toBeInTheDocument();
+    })
+
+    test('Deve Renderizar a rota cartoes', ()=>{
+        const rota = '/cartoes';
+        render(
+            <MemoryRouter initialEntries={[rota]}>
+                <Routes>
+                    <Route path="/" element={<App/>}>
+                        <Route path="cartoes" element={<Cartoes/>}/>
+                    </Route>
+                </Routes>
+            </MemoryRouter>
+        )
+        const meusCartoes = screen.getByText('Meus cartões')
+        expect(meusCartoes).toHaveTextContent('Meus cartões')
     })
 })

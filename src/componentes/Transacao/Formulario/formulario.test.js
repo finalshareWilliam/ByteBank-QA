9860a180cf9da1e781bb3ddda27e59b5f50e2488
William Souza <william.h.souza@hotmail.com>
2023-09-18 14:44:30 -0300

    v2.2

--- src/componentes/Transacao/Formulario/formulario.test.js
+++ src/componentes/Transacao/Formulario/formulario.test.js
@@ -2,50 +2,33 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Formulario from './index';
 
-describe('Deve renderizar um campo de input ', () => {
-  test('', () => {
+describe('Deve renderizar um campo de input', () => {
+  test('no documento', () => {
     render(<Formulario />);
-    const campoTexto = screen.getAllByPlaceholderText('Digite um valor');
-    campoTexto.forEach((elemento) => {
-      expect(elemento).toBeInTheDocument();
-    });
+    const campoTexto = screen.getByPlaceholderText('Digite um valor');
+    expect(campoTexto).toBeInTheDocument();
   });
 
-  test('com o type number', () => {
+  test(' com o type number', () => {
     render(<Formulario />);
-    const campoTexto = screen.getAllByPlaceholderText('Digite um valor');
-    campoTexto.forEach((elemento) => {
-      expect(elemento).toHaveAttribute('type', 'number');
-    });
+    const campoTexto = screen.getByPlaceholderText('Digite um valor');
+    expect(campoTexto).toHaveAttribute('type', 'number');
   });
 
-  test('que pode ser preenchido', () => {
+  test(' que pode ser preenchido', () => {
     render(<Formulario />);
     const campoTexto = screen.getByPlaceholderText('Digite um valor');
     userEvent.type(campoTexto, '50');
-    expect(Number(campoTexto.value)).toBe(50);
+    expect(campoTexto).toHaveValue(50);
   });
+});
 
-  test('Deve chamar um evento de onSubmit ao clicar em realizar transação', () => {
-    const realizarTransacao = jest.fn();
+test('Deve chamar um evento de onSubmit ao clicar em realizar transação', () => {
+  const realizarTransacao = jest.fn();
 
-    render(<Formulario realizarTransacao={realizarTransacao} />);
-    const botao = screen.getByRole('button');
-    userEvent.click(botao);
-    expect(realizarTransacao).toHaveBeenCalledTimes(1);
-  });
+  render(<Formulario realizarTransacao={realizarTransacao} />);
+  const botao = screen.getByRole('button');
 
-  test('Deve ser possivel selecionar uma opção do elemento <select/>', () => {
-    render(<Formulario />);
-    const select = screen.getByRole('combobox');
-    userEvent.selectOptions(select, ['Depósito']);
-
-    expect(
-      screen.getByRole('option', { name: 'Selecione um tipo de transação' })
-        .selected,
-    ).toBe(false);
-    expect(
-        screen.getByRole('option', { name: 'Depósito' }).selected).toBe(
-            true,);
-  });
+  userEvent.click(botao);
+  expect(realizarTransacao).toHaveBeenCalledTimes(1);
 });
--- src/utils/Utils.test.js
+++ src/utils/Utils.test.js
@@ -1,37 +1,37 @@
-import {calculaNovoSaldo} from './index'
+import { calculaNovoSaldo } from './index';
 
-describe('Quando realizo uma transação', ()=>{
-    test('Que é um depósito, o saldo deve aumentar',()=>{
-        const transacao = {
-            transacao: 'Depósito',
-            valor: 50
-        }
+describe('Quando eu realizo uma transação', () => {
+  it('Que é um depósito, o saldo deve aumentar', () => {
+    const transacao = {
+      transacao: 'Depósito',
+      valor: 50,
+    };
 
-        const novoSaldo = calculaNovoSaldo(transacao, 100)
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
 
-        expect(novoSaldo).toBe(150)
-    })
+    expect(novoSaldo).toBe(150);
+  });
 
-    test('Que é um transferencia, o saldo deve diminuir',()=>{
-        const transacao = {
-            transacao: 'Transferência',
-            valor: 50
-        }
+  test('Que é uma Transferência, o saldo deve diminuir', () => {
+    const transacao = {
+      transacao: 'Tranferência',
+      valor: 50,
+    };
 
-        const novoSaldo = calculaNovoSaldo(transacao, 100)
+    const novoSaldo = calculaNovoSaldo(transacao, 100);
 
-        expect(novoSaldo).toBe(50)
-    })
-})
+    expect(novoSaldo).toBe(50);
+  });
+});
 
-test('Deve retornar o valor do saldo atualizado com o rendimento', ()=>{
-    const calculaRendimento = jest.fn(saldo => saldo + saldo*0.005);
+it('Deve retornar o valor do saldo atualizado com o rendimento', () => {
+  const calculaRendimento = jest.fn((saldo) => saldo + saldo * 0.005);
 
-    const saldo = 100;
-    const novoSaldo = calculaRendimento(saldo)
+  const saldo = 100;
 
-    expect(novoSaldo).toBe(100.5)
-    expect(calculaRendimento).toBeCalled()
-    expect(calculaRendimento).toHaveBeenCalledWith(saldo)
-})
+  const novoSaldo = calculaRendimento(saldo);
 
+  expect(novoSaldo).toBe(100.5);
+  expect(calculaRendimento).toBeCalled();
+  expect(calculaRendimento).toHaveBeenCalledWith(saldo);
+});

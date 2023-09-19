6801ccb23eb3700356989d3d59255acb17462c72
William Souza <william.h.souza@hotmail.com>
2023-09-19 14:25:50 -0300

    v3.0

--- src/Routes.test.js
+++ src/Routes.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from './paginas/Principal/App';
+import AppRoutes from './routes'
 import { BrowserRouter, MemoryRouter, Routes, Route } from "react-router-dom";
 import Cartoes from './componentes/Cartoes';
 
@@ -23,5 +24,28 @@ describe('Rotas',()=>{
         )
         const meusCartoes = screen.getByText('Meus cartões')
         expect(meusCartoes).toHaveTextContent('Meus cartões')
+    })
+
+    test('Deve renderizar a localizacao da rota atual', ()=>{
+        const rota = '/cartoes';
+        render(
+            <MemoryRouter initialEntries={[rota]}>
+                <App/>
+            </MemoryRouter>
+        )
+        
+        const localizacaoAtual = screen.getByTestId('local')
+        expect(localizacaoAtual).toHaveTextContent(rota)
+    })
+
+    test('Deve Renderizar a pagina de erro 404', ()=>{
+        const rota = '/extrato';
+        render(
+            <MemoryRouter initialEntries={[rota]}>
+                <AppRoutes/>
+            </MemoryRouter>
+        )
+        const paginaErro = screen.getByTestId('pagina-404')
+        expect(paginaErro).toContainHTML('<h1>Ops! Não encontramos a página</h1>')
     })
 })
---
+++ src/paginas/Principal/App.test.js
@@ -0,0 +1,47 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"
+import App from './App'
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from '../../routes'
+
+describe('Componente <App/>', ()=>{
+    test('Deve permitir adicionar uma transação em Extrato', ()=>{
+        render(<App />, {wrapper: BrowserRouter})
+        
+        const select = screen.getByRole('combobox')
+        const campoValor = screen.getByPlaceholderText('Digite um valor')
+        const botao = screen.getByRole('button')
+
+        userEvent.selectOptions(select, ['Depósito'])
+        userEvent.type(campoValor, '100')
+        userEvent.click(botao)
+
+        const novaTransacao = screen.getByTestId('lista-transacoes')
+        const itemExtrato = screen.getByRole('listitem')
+
+        expect(novaTransacao).toContainElement(itemExtrato)
+    })
+
+    test('Deve navegar até a pagina correspondente ao link clicado', async ()=>{
+        render(<AppRoutes/>, {wrapper: BrowserRouter})
+
+        const linkPaginaCartoes = screen.getByText('Cartões')
+        expect(linkPaginaCartoes).toBeInTheDocument()
+
+        userEvent.click(linkPaginaCartoes)
+        const tituloPaginaCartoes = await screen.findByText('Meus cartões')
+        expect(tituloPaginaCartoes).toBeInTheDocument()
+    })
+
+    test('Deve navegar até a pagina investimentos ao link clicado', async ()=>{
+        render(<AppRoutes/>, {wrapper: BrowserRouter})
+
+        const linkPaginaInvestimentos = screen.getByText('Investimentos')
+        expect(linkPaginaInvestimentos).toBeInTheDocument()
+
+        userEvent.click(linkPaginaInvestimentos)
+        const tituloPaginaInvestimentos = await screen.findByText('Renda Fixa')
+        expect(tituloPaginaInvestimentos).toBeInTheDocument()
+    })
+
+})
